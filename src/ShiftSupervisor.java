6d89fbeefcc38342f23218874a2a0b3a401b95dc
Jeremy reed <jeremyreed1542@gmail.com>
2026-08-07 01:58:58 -0400

    ShiftSupervisor.java

--- src/ShiftSupervisor.java
+++ src/ShiftSupervisor.java
@@ -4,13 +4,12 @@
 
 public class ShiftSupervisor extends Employee {double annualSalary,productionBonus;
 
-    // Calling parent constructor
     public ShiftSupervisor(String name, String id, String date) {
         super(name, id, date);
     }
 
 
-// Mutator methods for the Shift Supervisors details
+    // Mutator methods
 
     public void setAnnualSalary(double annualSalary) {
         this.annualSalary = annualSalary;
@@ -19,7 +18,7 @@ public class ShiftSupervisor extends Employee {double annualSalary,productionBon
         this.productionBonus = productionBonus;
     }
 
-    // Returns a string representation of Shift Supervisor
+
     @Override
     public String toString() {
         return String.format(
@@ -30,18 +29,18 @@ public class ShiftSupervisor extends Employee {double annualSalary,productionBon
 
     public static void main(String[] args){
 
-// Objects created
+
         ShiftSupervisor worker1 = new ShiftSupervisor("Joe Jonas", "434-F", "10-31-2005");
         ShiftSupervisor worker2 = new ShiftSupervisor("John Wiginton","565-G","12-25-2005");
 
-// Calling setter methods
+
         worker1.setAnnualSalary(56000);
         worker1.setProductionBonus(7800);
 
         worker2.setAnnualSalary(75000);
         worker2.setProductionBonus(9000);
 
-// Printing data
+
         System.out.println(worker1);
         System.out.println(worker2);
     }
